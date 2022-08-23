928d3e83ba3b68e8bc1d4288f2f0fdb3abe714f3
Rafael Sousa <rafap.19@hotmail.com>
2022-08-22 21:23:08 -0300

    refactoring

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//All the glory for my lord Jesus Christ,who help me on creating my first to-do-list
 const add = document.querySelector('#submit').onclick = CreateListItem
 const ul = document.querySelector('#ul')
 const input = document.querySelector('#txt')
@@ -6,7 +7,8 @@ const tasks = JSON.parse(localStorage.getItem('tasks')) || []
 function CreateListItem(e){
     e.preventDefault()
 
-    function sendPromisse(){
+    const sendPromisse = ()=>{
+        //simulation of sending tasks for the database using promises
         const TASK_ADDED = new Promise((resolve,reject)=> input.value != '' ?
         resolve(`Item ${input.value} Added to the list!`)
        : reject('ERROR,please add an item to the list!')    
@@ -15,35 +17,28 @@ function CreateListItem(e){
     }
     sendPromisse()
 
-    function addItem() {
-        input.value !== '' ? createStorage() :showAlert()
-    }
+    const addItem = () => input.value !== '' ? createStorage() :showAlert()
     addItem()
     ul.innerHTML = ''
     Render()
     
 }
 
-
-function createStorage(){
+const createStorage = ()=>{
     tasks.push(input.value)
     input.value =''
     localStorage.setItem("tasks",JSON.stringify(tasks))
 }
 
+const showAlert = ()=> alert('Item is required!')
 
 
-function showAlert(){
-    alert('Item is required!')
-}
-
-document.getElementsByTagName('body').onload = Render
 function Render(){
     
     if(localStorage.tasks == null){
         ul.innerHTML = '<p>No Items</p>'
     }else{
-       tasks.forEach((task,index) => {
+       tasks.forEach((task) => { //forEach used for creation of dynamic tasks
         const div = document.createElement('div')
         div.setAttribute('class','flex')
         ul.appendChild(div)
@@ -52,21 +47,23 @@ function Render(){
         li.textContent = task
         div.appendChild(li)
         input.value = ''
-
+      
         const removeButton = document.createElement('i')
         removeButton.setAttribute('class','fa-regular fa-trash-can')
         div.appendChild(removeButton)
         removeButton.addEventListener('click',() =>{
-            ul.removeChild(div)
-            tasks.splice(index,1) 
-            localStorage.setItem("tasks",JSON.stringify(tasks))
+        //I do not use the "forEach index" because it was picking the wrong id
+            ul.removeChild(div) 
+            let index = tasks.indexOf(task)
+            tasks.splice(index,1)
+                localStorage.setItem("tasks",JSON.stringify(tasks))
         })
-
   });
  }
     
 }
 Render()
+
 
     
    
